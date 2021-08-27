e9c295a113f8255165743acc4890f516349b8e5f
LuizDeSa <luizdesa2012@gmail.com>
2021-08-26 21:38:23 -0300

    criando o app-curse-list.component e adicionando as imagens

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 //diz para o angular que essa classe será im component
 @Component({
@@ -6,8 +7,33 @@ import { Component } from "@angular/core";
   templateUrl: './course-list.component.html' //Entretando, o codigo fica mais organizado se criarmos um tamplate html para o component.
   //template: '<h2> Course List </h2>' //template statico -> caso o html não passe de  linhas, pode ser utilizado dessa forma.
 })
-export class CourseListComponent{ //deizando esta classe publica para que seja acessada em outros locais.
+export class CourseListComponent implements OnInit{ //deizando esta classe publica para que seja acessada em outros locais.
 
+  courses: Course[] = [];
 
+  ngOnInit():void{
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 99.99,
+        code: 'XPS-8796',
+        duration: 120,
+        rating: 4.5,
+        releaseDate: 'November, 2, 2019'
+      },
+      {
+        id: 2,
+        name: 'Angular: HTTP',
+        imageUrl: '',
+        price: 45.99,
+        code: 'LKL-1094',
+        duration: 80,
+        rating: 4,
+        releaseDate: 'December, 4, 2019'
+      }
+    ]
+  }
 
 }
