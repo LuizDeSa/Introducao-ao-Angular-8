9279f760e82a2bd5ac8158a4ac05209ab2f213ba
LuizDeSa <luizdesa2012@gmail.com>
2021-08-30 07:35:39 -0300

    Criando rotas com path variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CourseInfoComponent } from './courses/course-info.component';
 import { Erro404Component } from './erro-404/erro-404.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RepleacePipe } from './pipe/repleace.pipe';
@@ -17,17 +18,22 @@ import { RouterModule } from '@angular/router';
     StarComponent,
     RepleacePipe,
     NavBarComponent,
-    Erro404Component
+    Erro404Component,
+    CourseInfoComponent
   ],
   imports: [ // onde de fato nos importamos os nosso modulos externos e internos
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-      {
-        path: '', redirectTo:'courses', pathMatch: 'full' // rota padrão do angular
-      },
+
       {
         path: 'courses', component: CourseListComponent // quando utilizamor rotas, o selector perde o sentido, pois o componente agora é ativado com a rota e não mais com o selector;
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent // como não se trata de uma rota normal é necessario adicionar o nome de uma variavel no path variable
+      },
+      {
+        path: '', redirectTo:'courses', pathMatch: 'full' // rota padrão do angular
       },
       {
         path: '**', component: Erro404Component//quando não encontra a rota // rota padrão do angular
